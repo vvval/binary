4a71f9e220029bfe1c5ae89a0a15f596b36a2ce1
Valentin <vintsukevich@gmail.com>
2016-12-06 19:31:56 +0300

    Refactor

--- src/BinaryGenerator.php
+++ src/BinaryGenerator.php
@@ -16,10 +16,10 @@ use Vvval\Binary\Exceptions\WriteDataException;
 class BinaryGenerator
 {
     /**
-     * todo read about fopen
+     * Open file flags.
      */
-    const READ = 'rb';
-    const WRITE = 'wb';
+    const READ   = 'rb';
+    const WRITE  = 'wb';
     const APPEND = 'ab';
 
     private $filename = null;
@@ -28,149 +28,156 @@ class BinaryGenerator
     private $mode = self::READ;
 
     /**
-     * @param $compression
-     * @return $this
+     * Start process.
+     *
+     * @param string $filename
+     * @param string $mode
+     * @param bool   $compression
      * @throws HandlerException
      */
-    public function setCompression($compression)
+    public function start($filename, $mode, $compression = false)
     {
-        if (!empty($this->handler))
-        {
-            throw new HandlerException("Binary generator has already started with \"$this->filename\" file.");
+        if (!empty($this->handler)) {
+            throw new HandlerException("Binary generator has already been started, please finish it before next usage.");
         }
 
-        $this->compression = $compression;
-
-        return $this;
-    }
-
-    /**
-     * @param $filename
-     * @param $mode
-     * @throws HandlerException
-     */
-    public function start($filename, $mode)
-    {
         $this->mode = $mode;
         $this->filename = $filename;
+        $this->compression = $compression;
 
-        if (!empty($this->handler))
-        {
-            throw new HandlerException("Binary generator has already started with \"$this->filename\" file.");
-        }
+        $this->handler = $this->open();
 
-        if (!empty($this->compression))
-        {
-            $this->handler = gzopen($this->filename, $this->mode);
-        }
-        else
-        {
-            $this->handler = fopen($this->filename, $this->mode);
-        }
-
-        if (empty($this->handler))
-        {
+        if (empty($this->handler)) {
             throw new HandlerException("Error during opening \"$this->filename\" file.");
         }
     }
 
     /**
-     *
+     * Finish process.
      */
     public function finish()
     {
-        if (!empty($this->handler))
-        {
-            if (!empty($this->compression))
-            {
-                gzclose($this->handler);
-            }
-            else
-            {
-                fclose($this->handler);
-            }
-
+        if (!empty($this->handler)) {
+            $this->close();
             $this->handler = null;
         }
     }
 
     /**
-     * @param $data
+     * Write part of data into file.
+     *
+     * @param string $data
      * @return int
      * @throws HandlerException
      * @throws WriteDataException
      */
     public function writeData($data)
     {
-        if (!$this->isWriteMode())
-        {
+        if (!$this->isWriteMode()) {
             throw new WriteDataException("Unable to write data into \"$this->filename\" file, read mode is set.");
         }
 
-        if (empty($this->handler))
-        {
+        if (empty($this->handler)) {
             throw new HandlerException("Unable to write data into \"$this->filename\" file, no resource is available.");
         }
 
         $packedLength = pack('L', mb_strlen($data));
 
-        if (!empty($this->compression))
-        {
-            return gzwrite($this->handler, $packedLength . $data);
-        }
-
-        return fwrite($this->handler, $packedLength . $data);
+        return $this->write($packedLength, $data);
     }
 
     /**
+     * Write part of data.
+     *
      * @return null|string
      * @throws HandlerException
      * @throws ReadDataException
      */
     public function readData()
     {
-        if (!$this->isReadMode())
-        {
+        if (!$this->isReadMode()) {
             throw new ReadDataException("Unable to read data from \"$this->filename\" file, write mode is set.");
         }
 
-        if (empty($this->handler))
-        {
+        if (empty($this->handler)) {
             throw new HandlerException("Unable to read data from \"$this->filename\" file, no resource is available.");
         }
 
-        if (!empty($this->compression))
-        {
-            $length = gzread($this->handler, 4);
-        }
-        else
-        {
-            $length = fread($this->handler, 4);
-        }
+        $length = $this->read(4);
 
-        if (empty($length))
-        {
+        if (empty($length)) {
             return null;
         }
 
-        if (mb_strlen($length) != 4)
-        {
+        if (mb_strlen($length) != 4) {
             throw new ReadDataException("Unable to read data from \"$this->filename\" file, data is corrupted.");
         }
 
         $length = unpack('L', $length);
 
-        if (!empty($this->compression))
-        {
-            return gzread($this->handler, $length[1]);
-        }
-        else
-        {
-            return fread($this->handler, $length[1]);
+        return $this->read((int)$length);
+    }
+
+    /**
+     * Open file and get handler.
+     *
+     * @return resource
+     */
+    private function open()
+    {
+        if (!empty($this->compression)) {
+            return gzopen($this->filename, $this->mode);
+        } else {
+            return fopen($this->filename, $this->mode);
         }
     }
 
     /**
+     * Close handler file.
+     */
+    private function close()
+    {
+        if (!empty($this->compression)) {
+            gzclose($this->handler);
+        } else {
+            fclose($this->handler);
+        }
+    }
+
+    /**
+     * Read piece of data with the defined length.
+     *
+     * @param int $length
+     * @return string
+     */
+    private function read($length)
+    {
+        if (!empty($this->compression)) {
+            return gzread($this->handler, $length);
+        } else {
+            return fread($this->handler, $length);
+        }
+    }
+
+    /**
+     * Write data into file.
+     *
+     * @param int    $length
+     * @param string $data
+     * @return int
+     */
+    private function write($length, $data)
+    {
+        if (!empty($this->compression)) {
+            return gzwrite($this->handler, $length . $data);
+        } else {
+            return fwrite($this->handler, $length . $data);
+        }
+    }
+
+    /**
+     * If current file in read mode.
+     *
      * @return bool
      */
     private function isReadMode()
@@ -179,6 +186,8 @@ class BinaryGenerator
     }
 
     /**
+     * If current file in write mode.
+     *
      * @return bool
      */
     private function isWriteMode()
@@ -188,12 +197,10 @@ class BinaryGenerator
 
     /**
      * Check if file was closed.
-     *
      */
     public function __destruct()
     {
-        if (!empty($this->handler))
-        {
+        if (!empty($this->handler)) {
             throw new HandlerException("Binary generator hasn't finished \"$this->filename\" file yet.");
         }
     }
